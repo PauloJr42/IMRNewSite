bb4a9504e8dbf1a966b801909cf4c26a40c8fd18
PauloJr42 <97369090+PauloJr42@users.noreply.github.com>
2023-10-02 17:12:39 -0300

    up

---
+++ script.js
@@ -0,0 +1,25 @@
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o envio do formulário padrão
+  
+    var email = document.getElementById('email').value;
+  
+    // Objeto contendo os dados a serem enviados
+    var data = {
+      email: email
+    };
+  
+    // Requisição AJAX para enviar os dados para o arquivo PHP
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'form.php', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+  
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        // Exibe a resposta do servidor
+        console.log(xhr.responseText);
+      }
+    };
+  
+    // Envia os dados como JSON
+    xhr.send(JSON.stringify(data));
+  });
