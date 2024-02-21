b98ecb8f40eee3cf85a9cc33c507dc9c2c62e361
PauloJr42 <97369090+PauloJr42@users.noreply.github.com>
2024-02-20 23:07:00 -0300

    up

--- processar_formulario.php
+++ processar_formulario.php
@@ -22,36 +22,29 @@ try {
 } catch (PDOException $e) {
   die("Erro na conexão com o banco de dados PostgreSQL: " . $e->getMessage());
 }
-// Conexão com o PostgreSQL
-//try {
- // $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password");
-//} catch (PDOException $e) {
- // die("Erro na conexão com o banco de dados: " . $e->getMessage());
-//}
 
 // Processar formulário e salvar e-mail no banco de dados
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $email = $_POST['email'];
 
   // Verificar se o e-mail já existe na tabela (opcional)
-  $stmt = $pdo->prepare("SELECT * FROM emails WHERE email = ?");
+  $stmt = $conexao->prepare("SELECT * FROM emails WHERE email = ?");
   $stmt->execute([$email]);
 
   if ($stmt->rowCount() == 0) {
     // Inserir e-mail na tabela
-    $stmt = $pdo->prepare("INSERT INTO emails (email) VALUES (?)");
+    $stmt = $conexao->prepare("INSERT INTO emails (email) VALUES (?)");
     $stmt->execute([$email]);
     echo "E-mail cadastrado com sucesso!";
   } else {
     echo "Este e-mail já está cadastrado!";
   }
-}
-$stmt->execute([$email]);
 
-// Verificar se houve algum erro
-$errorInfo = $stmt->errorInfo();
-if ($errorInfo[0] != '00000') {
+  // Verificar se houve algum erro
+  $errorInfo = $stmt->errorInfo();
+  if ($errorInfo[0] != '00000') {
     die("Erro no banco de dados: " . $errorInfo[2]);
+  }
 }
 
 // Matando conexão
