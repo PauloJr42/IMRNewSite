200b1757d3c4cf2340a05de0ab99bc7b543c4512
PauloJr42 <paulosraiva@hotmail.com>
2024-12-11 03:00:29 -0300

    up

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ document.getElementById("capture-form").addEventListener("submit", async functio
     const phone = document.getElementById("phone").value;
 
     try {
-        const response = await fetch("https://imrservicos.netlify.app/.netlify/functions/capture", {
+        const response = await fetch("https://imrservicos.netlify.app", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
